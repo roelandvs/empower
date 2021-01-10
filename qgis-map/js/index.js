9f026c2adc17c593022a3747d6e7a6e9d80de875
Roeland <roelandsteevan@gmail.com>
2021-01-10 16:06:32 +0100

    style-fixes

--- qgis-map/js/index.js
+++ qgis-map/js/index.js
@@ -1,7 +1,6 @@
 let gemeenteNaam = document.getElementsByClassName("gemeente-titel");
+let sidebarTitle = document.querySelector(".sidebar-left h2");
 
 function myFunction() {
-	let sidebarTitle = document.querySelector(".sidebar-left h2");
 	sidebarTitle.innerText = gemeenteNaam[0].innerText;
-	// console.log(gemeenteNaam[0].innerText);
 };
