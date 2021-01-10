eddacc72202e836a1a4f65ff29fbb6733d01e215
Roeland <roelandsteevan@gmail.com>
2021-01-10 15:47:28 +0100

    added municipality title in sidebar

--- qgis-map/js/index.js
+++ qgis-map/js/index.js
@@ -1,1 +1,7 @@
-console.log('hey');
+let gemeenteNaam = document.getElementsByClassName("gemeente-titel");
+
+function myFunction() {
+	let sidebarTitle = document.querySelector(".sidebar-left h2");
+	sidebarTitle.innerText = gemeenteNaam[0].innerText;
+	// console.log(gemeenteNaam[0].innerText);
+};
