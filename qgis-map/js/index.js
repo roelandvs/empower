3b9518c04da91a391ba484b1c830c49ad20eec72
Roeland <roelandsteevan@gmail.com>
2021-01-13 23:01:25 +0100

    start year change module

--- qgis-map/js/index.js
+++ qgis-map/js/index.js
@@ -1,5 +1,6 @@
 let initiativeSidebar = document.querySelector(".sidebar");
-let menuSidebar = document.querySelector(".sidebar-menu");
+let buttonContainer = document.querySelector(".energy-buttons");
+let yearContainer = document.querySelector(".year-selector");
 
 function buildSidebar(gemeente) {
 	let shapeClass = document.querySelector(".sidebar-gemeente");
@@ -30,7 +31,8 @@ function buildSidebar(gemeente) {
 	autosTekst.innerText = document.getElementById('cars-s').innerText;
 
 	shapeClass.setAttribute('d', shapeValues);
-	menuSidebar.style.left = '250px';
+	buttonContainer.style.left = '240px';
+	yearContainer.style.left = '240px';
 	initiativeSidebar.style.left = '0';
 
 	//places the svg/path on point 0,0
@@ -45,8 +47,8 @@ function buildSidebar(gemeente) {
 
 function moveSidebar() {
 	initiativeSidebar.style.left = '-300px';
-	menuSidebar.style.left = '0';
-	console.log(datasetArray)
+	buttonContainer.style.left = '0';
+	yearContainer.style.left = '0';
 };
 
 function changeYear() {
@@ -57,13 +59,12 @@ function changeYear() {
 		style_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0_0(entry)
 	})
 
+	datasetArray = [];
+
 	map.createPane('pane_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0');
     var layer_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0 = new L.geoJson(json_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0, {
-        // attribution: '',
         interactive: true,
         dataVar: 'json_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0',
-        // layerName: 'layer_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0',
-        // pane: 'pane_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0',
         onEachFeature: pop_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0,
         style: style_Gemeentegrenzen_2019shpGemeentegrenzen__voorlopig____kustlijn_0_0,
     });
